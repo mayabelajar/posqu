6861205297cd9bfaf1ababae4e55f34fb04b7f1f
Petani Kode <contoh@petanikode.com>
2024-10-28 10:42:58 +0700

    Signed-off-by: Petani Kode <contoh@petanikode.com>

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RedirectController;
+use App\Http\Controllers\SuperadminController;
+use App\Http\Controllers\PegawaiController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+//  jika user belum login
+Route::group(['middleware' => 'guest'], function() {
+    Route::get('/', [AuthController::class, 'login'])->name('login');
+    Route::post('/', [AuthController::class, 'dologin']);
 
-Route::get('/', function () {
-    return view('welcome');
+});
+
+// untuk admin dan kasir
+Route::group(['middleware' => ['auth', 'checkrole:1,2']], function() {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/redirect', [RedirectController::class, 'cek']);
+});
+
+
+// untuk admin
+Route::group(['middleware' => ['auth', 'checkrole:1']], function() {
+    Route::get('/admin', [AdminController::class, 'index']);
+});
+
+// untuk kasir
+Route::group(['middleware' => ['auth', 'checkrole:2']], function() {
+    Route::get('/kasir', [KasirController::class, 'index']);
+
 });
