fcf9d3e666b7b55e1a6f7d1240cd344d4638c1e4
mayabelajar <yandatau2023@gmail.com>
2024-11-25 09:16:09 +0700

    Signed-off-by: mayabelajar <yandatau2023@gmail.com>

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produk;
 
 class AdminController extends Controller
 {
@@ -11,7 +12,8 @@ class AdminController extends Controller
     }
 
     public function admin() {
-        return view('admin.template');
+        $produks = Produk::all();
+        return view('admin.template', compact('produks'));
     }
     
     public function transaksi() {
--- resources/views/admin/template.blade.php
+++ resources/views/admin/template.blade.php
@@ -56,112 +56,26 @@
       <div class="card--container shadow mt-4"> <!-- card container -->
         <h3 class="main--title">Hidangan Populer</h3>
           <div class="card--wrapper"> <!-- card wrapper -->
+          
             <div class="container text-center"> <!-- container -->
-              <div class="row"> <!-- row -->
+              <div class="row"> <!--row -->
+              @foreach ($produks as $data)
                 <div class="col-3"> <!-- col -->
                   <div class="card" style="width: 100%;"> <!-- card -->
-                    <img src="{{ asset('/lte/dist/img/rujakcingur.jpeg') }}" class="rounded-circle mx-auto my-3" width="100px" alt="...">
+                    <img src="{{ asset('/storage/app/public/produks/'.$data->image) }}" class="rounded-circle mx-auto my-3" width="100px" alt="">
                     <div class="card-body"> <!-- card body -->
-                      <h5 class="card-title">Rujak Cingur</h5>
-                      <p class="card-text">Makanan khas Jawa Timur. Perpaduan antara sayuran, buah, tahu, tempe, dan petis yang diuleg bersama petis dan kacang.</p>
-                      <h5 class="card-title">Rp12.000</h5>
+                      <h5 class="card-title">{{$data->nama}}</h5>
+                      <p class="card-text">{{$data->deskripsi}}</p>
+                      <h5 class="card-title">Rp{{$data->harga}}</h5>
                       <button type="button" class="btn-text btn btn-success">Beli Sekarang</button>
                       <button type="button" class="btn btn-icon"><i class="fa fa-cart-plus" aria-hidden="true"></i></button>
                     </div> <!-- card body -->
                   </div> <!-- card -->
                 </div> <!-- col -->
-                <div class="col-3"> <!-- col -->
-                  <div class="card" style="width: 100%;"> <!-- card -->
-                    <img src="{{ asset('/lte/dist/img/esdawet.jpeg') }}" class="rounded-circle mx-auto my-3" width="100px" alt="...">
-                    <div class="card-body"> <!-- card body -->
-                      <h5 class="card-title">Es Dawet</h5>
-                      <p class="card-text">Minuman khas Jawa Tengah. Minuman ini memiliki rasa manis yang berasal dari gula merah dan rasa gurih yang berasal dari santan.</p>
-                      <h5 class="card-title">Rp8.000</h5>
-                      <button type="button" class="btn-text btn btn-success">Beli Sekarang</button>
-                      <button type="button" class="btn btn-icon"><i class="fa fa-cart-plus" aria-hidden="true"></i></button>
-                    </div> <!-- card body -->
-                  </div> <!-- card -->
-                </div> <!-- col -->
-                <div class="col-3"> <!-- col -->
-                  <div class="card" style="width: 100%;"> <!-- card -->
-                    <img src="{{ asset('/lte/dist/img/lumpia.jpeg') }}" class="rounded-circle mx-auto my-3" width="100px" alt="...">
-                    <div class="card-body"> <!-- card body -->
-                      <h5 class="card-title">Lumpia</h5>
-                      <p class="card-text">Makanan khas Jawa Tengah. Lumpia terbuat dari rebung sebagai isiannya dan digulung oleh kulit yang terbuat dari tepung.</p>
-                      <h5 class="card-title">Rp10.000</h5>
-                      <button type="button" class="btn-text btn btn-success">Beli Sekarang</button>
-                      <button type="button" class="btn btn-icon"><i class="fa fa-cart-plus" aria-hidden="true"></i></button>
-                    </div> <!-- card body -->
-                  </div> <!-- card -->
-                </div> <!-- col -->
-                <div class="col-3"> <!-- col -->
-                  <div class="card" style="width: 100%;"> <!-- card -->
-                    <img src="{{ asset('/lte/dist/img/rendang.jpeg') }}" class="rounded-circle mx-auto my-3" width="100px" alt="...">
-                    <div class="card-body"> <!-- card body -->
-                      <h5 class="card-title">Rendang</h5>
-                      <p class="card-text">Makanan khas Sumatera Barat. Perpaduan antara daging dan rempah nusantara, yang dimasak 12 jam lamanya.</p>
-                      <h5 class="card-title">Rp35.000</h5>
-                      <button type="button" class="btn-text btn btn-success">Beli Sekarang</button>
-                      <button type="button" class="btn btn-icon"><i class="fa fa-cart-plus" aria-hidden="true"></i></button>
-                    </div> <!-- card body -->
-                  </div> <!-- card -->
-                </div> <!-- col -->
-              </div><!-- row -->
-            </div> <!-- container -->
-          </div> <!-- card wrapper -->
-          <div class="card--wrapper"> <!-- card wrapper -->
-            <div class="container text-center"> <!-- container -->
-              <div class="row"> <!-- row -->
-                <div class="col-3"> <!-- col -->
-                  <div class="card" style="width: 100%;"> <!-- card -->
-                    <img src="{{ asset('/lte/dist/img/ayambetutu.jpeg') }}" class="rounded-circle mx-auto my-3" width="100px" alt="...">
-                    <div class="card-body">  <!-- card body -->
-                      <h5 class="card-title">Ayam Betutu</h5>
-                      <p class="card-text">Makanan khas Bali. Ayam utuh yang dimasak dengan bumbu rempah khas Bali kemudian dipanggang dengan api sekam.</p>
-                      <h5 class="card-title">Rp12.000</h5>
-                      <button type="button" class="btn-text btn btn-success">Beli Sekarang</button>
-                      <button type="button" class="btn btn-icon"><i class="fa fa-cart-plus" aria-hidden="true"></i></button>
-                    </div> <!-- card body -->
-                  </div> <!-- card -->
-                </div> <!-- col -->
-                <div class="col-3"> <!-- col -->
-                  <div class="card" style="width: 100%;"> <!-- card -->
-                    <img src="{{ asset('/lte/dist/img/papeda.jpeg') }}" class="rounded-circle mx-auto my-3" width="100px" alt="...">
-                    <div class="card-body"> <!-- card body -->
-                      <h5 class="card-title">Papeda</h5>
-                      <p class="card-text">Papeda adalah yang berasal dari Maluku dan pesisir Papua. Makanan ini berupa bubur sagu dan biasanya disajikan dengan ikan tongkol.</p>
-                      <h5 class="card-title">Rp25.000</h5>
-                      <button type="button" class="btn-text btn btn-success">Beli Sekarang</button>
-                      <button type="button" class="btn btn-icon"><i class="fa fa-cart-plus" aria-hidden="true"></i></button>
-                    </div> <!-- card body -->
-                  </div> <!-- card -->
-                </div> <!-- col -->
-                <div class="col-3"> <!-- col -->
-                  <div class="card" style="width: 100%;"> <!-- card -->
-                    <img src="{{ asset('/lte/dist/img/wedanguwuh.jpeg') }}" class="rounded-circle mx-auto my-3" width="100px" alt="...">
-                    <div class="card-body"> <!-- card body -->
-                      <h5 class="card-title">Wedang Uwuh</h5>
-                      <p class="card-text">Minuman Khas Yogyakarta. Minuman tradisional yang terbuat dengan rempah-rempah untuk menyehatkan badan.</p>
-                      <h5 class="card-title">Rp10.000</h5>
-                      <button type="button" class="btn-text btn btn-success">Beli Sekarang</button>
-                      <button type="button" class="btn btn-icon"><i class="fa fa-cart-plus" aria-hidden="true"></i></button>
-                    </div> <!-- card body -->
-                  </div> <!-- card -->
-                </div> <!-- col -->
-                <div class="col-3"> <!-- col -->
-                  <div class="card" style="width: 100%;"> <!-- card -->
-                    <img src="{{ asset('/lte/dist/img/mieaceh.jpeg') }}" class="rounded-circle mx-auto my-3" width="100px" alt="...">
-                    <div class="card-body"> <!-- card body -->
-                      <h5 class="card-title">Mie Aceh</h5>
-                      <p class="card-text">Makanan khas Aceh. Mie dengan ukuran tebal berwarna kuning yang dimasak dengan bumbu kari dan diberi topping seafood.</p>
-                      <h5 class="card-title">Rp15.000</h5>
-                      <button type="button" class="btn-text btn btn-success">Beli Sekarang</button>
-                      <button type="button" class="btn btn-icon"><i class="fa fa-cart-plus" aria-hidden="true"></i></button>
-                    </div> <!-- card body -->
-                  </div> <!-- card -->
-                </div> <!-- col -->
+                @endforeach
               </div> <!-- row -->
             </div> <!-- container -->
+            
           </div> <!-- card wrapper -->
         </div> <!-- card container -->
       <!-- </div> main content -->
