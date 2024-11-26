cade3f76130f15f50fe458f90f38d09319802adc
mayabelajar <yandatau2023@gmail.com>
2024-11-26 21:11:11 +0700

    Signed-off-by: mayabelajar <yandatau2023@gmail.com>

--- resources/views/admin/template.blade.php
+++ resources/views/admin/template.blade.php
@@ -80,12 +80,12 @@
         </div> <!-- card container -->
       <!-- </div> main content -->
 
-      <aside class="control-sidebar control-sidebar-light">
-    <div class="p-3">
-        <div class="keranjang">
-            <h4><b>Keranjang</b><h4>
-        </div>
-        <title>Keranjang Belanja</title>
+<aside class="control-sidebar control-sidebar-light">
+  <div class="p-3">
+    <div class="keranjang">
+      <h4><b>Keranjang</b><h4>
+    </div>
+    <title>Keranjang Belanja</title>
 
         <style>
             /* Gaya CSS di sini */
@@ -108,31 +108,7 @@
 
 
 
-        <div class="pemesanan mb-4">
-        @foreach ($produks as $data)
-        <div class="flex justify-between items-center mb-2">
-            <img class="gambar1" src="{{ asset('/storage/produks/'.$data->image) }}">
-            <span>{{$data->nama}}</span>
-            <button class="krj"><i class="fa fa-plus-circle"></i></button>
-            <button class="krj"><i class="fa fa-minus-circle"></i></button>
-            <span>{{$data->harga}}</span>
-        </div>
-        @endforeach
-    </div>
-    <div class="notes">
-        <label for="notes">Catatan :</label>
-        <textarea class="form-control mb-4" name="notes" rows="4"></textarea>
-    </div>
-    <div class="border-t border-gray-250 mb-3">
-    </div>
-    <div class="flex justify-between mb-3">
-      <ul class="listCard"></ul>
-      <div class="checkout"></div>
-            <div class="total">0</div>
-     </div>
-     <div>
-     <button type="button"><a href="{{ url('/payment') }}" class="proses">Proses Transaksi</a></button>
-     </div>
+
      
     <script src="ini.js">
         // JavaScript untuk menghitung total, mengelola diskon, dll. 
@@ -155,12 +131,50 @@
           }
           localStorage.setItem('keranjang', JSON.stringify(storageproduk));
           console.log("Product added to cart:", product);
-        })
-      })
-    </script>
+          displayCart();
+        });
 
-    
+        function displayCart() {
+          var storageproduk = JSON.parse(localStorage.getItem('keranjang')) || [];
+          var cartDiv = $('.keranjang');
+          cartDiv.empty();
+          if (storageproduk.length === 0) {
+                    cartDiv.append('<p>Keranjang Anda kosong.</p>');
+          } else {
+            storageproduk.forEach(function(item) {
+              cartDiv.append(`
+                <div class="pemesanan mb-4">
+                <div class="flex justify-between items-center mb-2">
+                  <img class="gambar1" src="${item.image}">
+                  <span>${item.nama}</span>
+                  <button class="krj"><i class="fa fa-plus-circle"></i></button>
+                  <button class="krj"><i class="fa fa-minus-circle"></i></button>
+                  <span>${item.harga}</span>
+                </div>
+                </div>
+                <div class="notes">
+                  <label for="notes">Catatan :</label>
+                  <textarea class="form-control mb-4" name="notes" rows="4"></textarea>
+                </div>
+                <div class="border-t border-gray-250 mb-3"></div>
+                <div class="flex justify-between mb-3">
+                  <ul class="listCard"></ul>
+                  <div class="checkout"></div>
+                    <div class="total">0</div>
+                </div>
+                <div>
+                    <button type="button"><a href="{{ url('/payment') }}" class="proses">Proses Transaksi</a></button>
+                </div>
+              `);
+            });
+          }
+        }
+        displayCart();
+      });
+    </script>
 </aside>
+
+
     <!-- Modal -->
     <!-- <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
