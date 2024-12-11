26c45f52ab44404a65772655087b9a3c78150ccc
Petani Kode <contoh@petanikode.com>
2024-12-11 16:08:38 +0700

    Signed-off-by: Petani Kode <contoh@petanikode.com>

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -24,10 +24,11 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link px-center">
-      <img src="{{ asset('/lte/dist/img/logoPOS.png') }}" alt="POSq Logo" class="img-circle elevation-3"
-           style="opacity: .8">
-    </a>
+    <div style="text=align: center;">
+      <a href="#" class="brand-link">
+        <img src="{{ asset('/lte/dist/img/logoPOS.png') }}" alt="POSq Logo" style="opacity: .8; display: block; margin: 0 auto;">
+      </a>
+    </div>
 
     <!-- Sidebar -->
     <div class="sidebar">
