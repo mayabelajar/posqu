df455ae5398ee5fbfbac325c829574e3ff4d9002
mayabelajar <yandatau2023@gmail.com>
2024-12-18 16:14:08 +0700

    Signed-off-by: mayabelajar <yandatau2023@gmail.com>

--- resources/views/produk/index.blade.php
+++ resources/views/produk/index.blade.php
@@ -69,30 +69,6 @@
                       @endforeach
                     </tbody>
                 </table>
-                <!-- <div class="flex justify-between items-center mt-4">
-                  <span>
-                    1 - 10 dari 68 data
-                  </span>
-                  <nav aria-label="...">
-                    <ul class="pagination">
-                      <li class="page-item disabled">
-                        <a class="page-link">Previous</a>
-                      </li>
-                      <li class="page-item">
-                        <a class="page-link" href="#">1</a>
-                      </li>
-                      <li class="page-item active" aria-current="page">
-                        <a class="page-link" href="#">2</a>
-                      </li>
-                      <li class="page-item">
-                        <a class="page-link" href="#">3</a>
-                      </li>
-                      <li class="page-item">
-                        <a class="page-link" href="#">Next</a>
-                      </li>
-                    </ul>
-                  </nav>
-                </div> -->
             </div>
         </div>
     </div>
